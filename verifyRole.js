be7766706fdf902466d997396e1181a5042ddc73
Arihafi <arihafipatrick@gmail.com>
2023-01-13 06:36:34 +0200

    fixing

--- verifyRole.js
+++ verifyRole.js
@@ -3,7 +3,7 @@ const User = require('./models/User');
 
 //verifying Admin
 exports.isAdmin = (req, res,next) =>{
-  if(req.user.role != roles.admin){
+  if(req.user.role !== roles.admin){
     return res.status(401).send('Acess Denied, You must be an Admin');;
   } 
   next();
